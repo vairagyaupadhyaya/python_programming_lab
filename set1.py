11bdfae69cb5f2798a848811be6f6acff367d699
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 10:17:48 +0530

    Write a Python program to add member(s) in a set.

---
+++ set1.py
@@ -0,0 +1,9 @@
+#A new empty set
+color_set = set()
+print(color_set)
+print("\nAdd single element:")
+color_set.add("Red")
+print(color_set)
+print("\nAdd multiple items:")
+color_set.update(["Blue", "Green"])
+print(color_set)
