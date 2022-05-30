7d10c1ef45bdfc608eee6863dd81fb2b48dc1a17
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 11:30:04 +0530

    check if a number is even or odd

---
+++ function5.py
@@ -0,0 +1,7 @@
+num = int (input ('Enter any number to test whether it is odd or even:')
+
+ def odd_or_even(num):
+    if (num % 2) == 0:
+        print ('The number is even')
+    else: 
+        print ('The provided number is odd')
