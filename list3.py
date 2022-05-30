cf901b6f2302252ba1550ae05eea6d94a5880e6e
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 10:12:31 +0530

    Sort the list by the length of string elements

---
+++ list3.py
@@ -0,0 +1,6 @@
+## initializing the list of strings
+strings = ["hafeez", "aslan", "honey", "appi"]
+## using sorted(key) function along with the key len
+sorted_list = list(sorted(strings, key = len))
+## printing the strings after sorting
+print(sorted_list)
