341426217bdde5bc922fc94d9e4d89d785d6ab2f
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 10:36:21 +0530

    Write a function to calculate area and perimeter of a rectangle.

---
+++ function1.py
@@ -0,0 +1,10 @@
+def Areaofrectangle(width, height) :
+Area = width * height
+Perimeter = 2
+* (width + height)
+print ("Area of a rectangle is: %.2f" %Area)
+print ("Perimeter of rectangle is: %.2f" %Perimeter)
+width = float (input (' Enter the width of a rectangle:
+"))
+height = float (input ('Enter the height of a rectangle: '))
+Areaofrectangle/width, height)I
