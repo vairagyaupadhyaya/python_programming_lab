0b9e202ea7a164f4fd780d2b590eda435b0de63b
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 11:33:08 +0530

    Write a Python program to set a random seed and get a random number between 0 and 1. Use random.random.

---
+++ module3.py
@@ -0,0 +1,11 @@
+import random 
+print("Set a random seed and get a random number between 0 and 1:")
+random.seed(0)
+new_random_value = random.random()
+print(new_random_value)
+random.seed(1)
+new_random_value = random.random()
+print(new_random_value)
+random.seed(2)
+new_random_value = random.random()
+print(new_random_value)
