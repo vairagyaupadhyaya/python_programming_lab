b3fbc25ac60ef1a3ded6316a0363b92e6ebcfa61
vairagyaupadhyaya <93581452+vairagyaupadhyaya@users.noreply.github.com>
2022-05-30 10:24:29 +0530

    Write a Python program to create an intersection of sets

---
+++ set4.py
@@ -0,0 +1,8 @@
+setx = set(["green", "blue"])
+sety = set(["blue", "yellow"])
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx & sety
+print(setz)
